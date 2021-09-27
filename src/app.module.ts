a01eb107a8166fe29ecd36cfd21efb61de69e07a
민경환 <ber01@naver.com>
2021-09-27 17:32:01 +0900

    chore(deps): TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,32 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm'
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
 import { UsersModule } from './users/users.module'
 
+const typeOrmModuleOptions = {
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => ({
+    namingStrategy: new SnakeNamingStrategy(),
+    type: 'postgres',
+    host: configService.get('DATABASE_HOST'),
+    password: configService.get('DATABASE_PASSWORD'),
+    port: configService.get('DATABASE_PORT'),
+    username: configService.get('DATABASE_USER'),
+    database: configService.get('DATABASE_DATABASE'),
+    autoLoadEntities: true,
+    logging: ['error', 'query'],
+    synchronize: true,
+  }),
+  inject: [ConfigService],
+}
+
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+    UsersModule,
+  ],
 })
 export class AppModule {}
