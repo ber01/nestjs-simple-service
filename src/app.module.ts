56c53afb2be78ce02ceeaf6bc350d35727931423
민경환 <ber01@naver.com>
2021-09-27 17:29:22 +0900

    chore(deps): ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { UsersModule } from './users/users.module'
 
 @Module({
-  imports: [UsersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
 })
 export class AppModule {}
