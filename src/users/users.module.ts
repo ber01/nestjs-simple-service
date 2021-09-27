201c5864bf86f3d810742b8463bb6e369027ec67
민경환 <ber01@naver.com>
2021-09-27 17:54:03 +0900

    feat(users): TypeOrmModule import & export

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { UsersController } from './users.controller'
+import { User } from './users.entity'
+import { UserRepository } from './users.repository'
 import { UsersService } from './users.service'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, UserRepository])],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [TypeOrmModule],
 })
 export class UsersModule {}
