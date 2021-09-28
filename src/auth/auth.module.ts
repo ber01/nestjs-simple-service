57560ae965de00a9c588094c1aae8878b0d6323e
민경환 <ber01@naver.com>
2021-09-28 17:42:48 +0900

    feat(auth): import PassportModule, JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { JwtModule, JwtModuleAsyncOptions } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
 import { AuthService } from './auth.service'
+import { JwtStrategy } from './strategies'
+
+const jwtModuleAsyncOptions: JwtModuleAsyncOptions = {
+  useFactory: async (configService: ConfigService) => ({
+    secret: configService.get('JWT_SECRET_KEY'),
+    signOptions: { expiresIn: '10m' },
+  }),
+  inject: [ConfigService],
+}
 
 @Module({
-  providers: [AuthService],
+  imports: [PassportModule, JwtModule.registerAsync(jwtModuleAsyncOptions)],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
