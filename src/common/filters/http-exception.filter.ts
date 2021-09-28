1c8db1cbc33540d8623ecff82d28e3195d0b6678
민경환 <ber01@naver.com>
2021-09-28 14:38:03 +0900

    feat(filters): httpExceptionFilter

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,27 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common'
+import { Response } from 'express'
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp()
+    const response = ctx.getResponse<Response>()
+    const status = exception.getStatus()
+    const res = exception.getResponse() as
+      | string
+      | { error: string; statusCode: number; message: string | string[] }
+
+    response.status(status).json({
+      statusCode: status,
+      message: typeof res === 'string' ? res : res.message,
+      data: null,
+    })
+  }
+}
+
+export const httpExceptionFilter = new HttpExceptionFilter()
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { httpExceptionFilter } from './common/filters'
 import { validationPipe } from './common/pipes'
 
 const logger = new Logger(bootstrap.name)
@@ -8,6 +9,7 @@ const logger = new Logger(bootstrap.name)
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(validationPipe)
+  app.useGlobalFilters(httpExceptionFilter)
 
   await app.listen(3000)
   logger.log('Hello World!')
