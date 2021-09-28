8816515e6fa4a55b4fb3c8d12214b4eecb4a5e84
민경환 <ber01@naver.com>
2021-09-29 08:16:56 +0900

    refactor(strategies): JwtStrategy validate 반환값 변경

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { PassportStrategy } from '@nestjs/passport'
 import { ExtractJwt, Strategy } from 'passport-jwt'
+import { UserRepository } from 'src/users/users.repository'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly userRepository: UserRepository,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,6 +18,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   public async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username }
+    const userId = payload.sub
+    return await this.userRepository.findByIdWithoutPassword(userId)
   }
 }
