d8d1ce19882d5b803110f865e09615d13ee79f39
민경환 <ber01@naver.com>
2021-09-28 15:40:42 +0900

    feat(interceptors): SuccessInterceptor

---
+++ src/common/interceptors/success.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common'
+import { Response } from 'express'
+import { map, Observable } from 'rxjs'
+
+@Injectable()
+export class SuccessInterceptor implements NestInterceptor {
+  intercept(ctx: ExecutionContext, next: CallHandler): Observable<any> {
+    const res = ctx.switchToHttp().getResponse<Response>()
+    const statusCode = res.statusCode
+    return next.handle().pipe(
+      map((data) => ({
+        statusCode,
+        message: null,
+        data,
+      })),
+    )
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common'
+import { SuccessInterceptor } from 'src/common/interceptors'
 import { SignUpRequestDto } from './dtos'
 import { UsersService } from './users.service'
 
 @Controller('users')
+@UseInterceptors(SuccessInterceptor)
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
