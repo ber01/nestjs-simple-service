e38c0c2ff722e6e16b5bc979337f9d28f33fdddc
민경환 <ber01@naver.com>
2021-09-28 18:08:17 +0900

    feat(users): login

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,8 @@ import {
   Post,
   UseInterceptors,
 } from '@nestjs/common'
+import { AuthService } from 'src/auth/auth.service'
+import { LoginRequestDto } from 'src/auth/dtos'
 import { SuccessInterceptor } from 'src/common/interceptors'
 import { SignUpRequestDto } from './dtos'
 import { UsersService } from './users.service'
@@ -13,7 +15,10 @@ import { UsersService } from './users.service'
 @Controller('users')
 @UseInterceptors(SuccessInterceptor)
 export class UsersController {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Post()
   public signUp(@Body() dto: SignUpRequestDto) {
@@ -28,5 +33,10 @@ export class UsersController {
   @Get('/:id')
   public getUserById(@Param('id') id: string) {
     return this.userService.getUserById(id)
+  }
+
+  @Post('/login')
+  public login(@Body() dto: LoginRequestDto) {
+    return this.authService.login(dto)
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common'
+import { forwardRef, Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { AuthModule } from 'src/auth/auth.module'
 import { UsersController } from './users.controller'
 import { User } from './users.entity'
 import { UserRepository } from './users.repository'
 import { UsersService } from './users.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([User, UserRepository]),
+    forwardRef(() => AuthModule),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [TypeOrmModule],
