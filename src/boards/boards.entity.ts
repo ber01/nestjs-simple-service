22c11be571e57a2fd237ca403327f53e19fa6834
민경환 <ber01@naver.com>
2021-11-12 15:25:10 +0900

    feat(entity): User, Board relations 설정

--- src/boards/boards.entity.ts
+++ src/boards/boards.entity.ts
@@ -1,6 +1,7 @@
 import { IsNotEmpty, IsString } from 'class-validator'
 import { BaseEntity } from 'src/common/entities'
-import { Column, Entity } from 'typeorm'
+import { User } from 'src/users/users.entity'
+import { Column, Entity, ManyToOne } from 'typeorm'
 
 @Entity()
 export class Board extends BaseEntity {
@@ -16,4 +17,7 @@ export class Board extends BaseEntity {
 
   @Column({ type: 'varchar', default: [] })
   public tag!: string[]
+
+  @ManyToOne(() => User, (user) => user.boards)
+  public user!: User
 }
--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,6 +1,7 @@
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator'
+import { Board } from 'src/boards/boards.entity'
 import { BaseEntity } from 'src/common/entities'
-import { Column, Entity } from 'typeorm'
+import { Column, Entity, OneToMany } from 'typeorm'
 
 @Entity()
 export class User extends BaseEntity {
@@ -22,4 +23,7 @@ export class User extends BaseEntity {
   @IsOptional()
   @Column({ type: 'varchar', nullable: true })
   public imageUrl?: string
+
+  @OneToMany(() => Board, (board) => board.user, { cascade: true })
+  public boards!: Board[]
 }
